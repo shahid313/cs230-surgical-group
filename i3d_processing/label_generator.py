2cbeebf7c4b9010ad15d67a7669f770e0b902ed5
Suraj Menon <sjmickiemouse@gmail.com>
2019-06-03 23:34:44 -0700

    ..

--- i3d_processing/label_generator.py
+++ i3d_processing/label_generator.py
@@ -75,7 +75,8 @@ def run_processing():
                     label_f.readline()
 
                 #if true, exit the loops
-                if (((minute+2) >= last_min) and (hour == 1)):
+
+                if (((minute+2) >= last_min) and (hour == last_hour)):
                     hour = last_hour
                     minute = 60
 
