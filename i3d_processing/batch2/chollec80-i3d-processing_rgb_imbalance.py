5809634ed4fffafe34767b4fd17b1b2fa2406d9c
Suraj Menon <sjmickiemouse@gmail.com>
2019-06-08 11:14:13 -0700

    ..

---
+++ i3d_processing/batch2/chollec80-i3d-processing_rgb_imbalance.py
@@ -0,0 +1,154 @@
+import os
+import subprocess
+import numpy as np
+import cv2
+
+def run_processing():
+
+    dirs = set([])
+    video_ids = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", 
+                 "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+                 "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+                 "31", "32", "33", "34", "35", "36", "37", "38", "39", "40",
+                 "41", "42", "43", "44", "45", "46", "47", "48", "49", "50",
+                 "51", "52", "53", "54", "55", "56", "57", "58", "59", "60"]
+    root_dir = "chollec80_raw_data"
+    destination_dir = "chollec80_processed_data_full_batch2"
+
+    imbalanced_classes = [0, 2, 4, 5, 6]
+
+    #loop through all  videos
+
+    for video_id in video_ids:
+
+        subprocess.call(["rm", "-rf", "proc"])
+        subprocess.call(["mkdir", "proc"])
+
+        #video preprocess (should be higher dimension, but cut like this for now)
+        subprocess.call(["ffmpeg", "-i", root_dir + "/video" + video_id + ".mp4", "-r", "5", "-s", "224x224", "-aspect", "4:3", "proc/video_process.mp4"])
+
+        label_txt = root_dir + "/video" + video_id + "-timestamp.txt"
+
+        #get hour and minute bounds
+        time_limit_list = list(open(label_txt, 'r'))
+        time_limit_len = len(time_limit_list)
+
+        time_limit_f = open(label_txt, "r")
+
+        for l in range(1, time_limit_len):
+            time_limit_f.readline()
+
+        limit_line = time_limit_f.readline()
+        info = limit_line.split()
+        last_time = info[0]
+
+        last_hour = int(last_time[0] + last_time[1])
+        last_min = int(last_time[3] + last_time[4])
+
+        print (last_hour)
+        print (last_min)
+
+        label_finder = open(label_txt, "r")
+        label_finder.readline()
+
+        video_done = False
+
+        for hour in range(0, (last_hour+1), 1):
+            for minute in range(0, (60), 1):
+                for second in range(0, (60), 10):
+
+                    label_line = label_finder.readline()
+                    info = label_line.split()
+
+                    label = int(label_decoder(info[1]))
+
+                    if ((label in imbalanced_classes) and (second != 30)):
+
+                        subprocess.call(["rm", "-rf", "tmp"])
+                        subprocess.call(["mkdir", "tmp"])
+
+                        if ((second+10) == 60 and (minute+1) == 60):
+                            next_hour = hour+1
+                            next_min = 0
+                            next_second = 0
+                        elif ((second+10) == 60):
+                            next_hour = hour
+                            next_min = minute+1
+                            next_second = 0
+                        else:
+                            next_hour = hour
+                            next_min = minute
+                            next_second = second+10
+
+
+                    # cut videos in 10s samples
+                    subprocess.call(["ffmpeg", "-i", "proc/video_process.mp4", "-ss", ('%02d' % hour)+":"+('%02d' % minute)+":"+('%02d' % second), "-to", ('%02d' % next_hour)+":"+('%02d' % next_min)+":"+('%02d' % next_second), "tmp/video_process_cropped.mp4"])
+
+
+                    #sample the vids into 5 fps
+                    subprocess.call(["ffmpeg", "-i", "tmp/video_process_cropped.mp4", "tmp/thumb%04d.jpg"])
+
+                    #loop through all images, 5 fps for 10s
+                    for i in range(1, 51):
+
+                        image_str = "tmp/thumb" + ('%04d' % i) + ".jpg"
+                        image = cv2.imread(image_str, cv2.IMREAD_COLOR)
+
+                        #handle RGB
+                        norm_image_rgb = cv2.normalize(image, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+
+                        #Crop 224x224 samples
+
+                        #reshape
+                        norm_image_rgb_r = norm_image_rgb.reshape(1, 224, 224, 3)
+
+                        #Add to RGB numpy
+                        if ( i == 1 ):
+                            rgb_video = norm_image_rgb_r
+                        else:
+                            rgb_video = np.concatenate((rgb_video, norm_image_rgb_r), axis=0)
+
+                        print ("Processed image " + str(i))
+
+
+                    rgb_video_r = rgb_video.reshape(1, 50, 224, 224, 3)
+
+                    #when done, copy .npy files for one 10s video clip, RGB and Flow
+                    destination_rgb = destination_dir + "/video_rgb" + video_id + "_" + str(hour) + "_" + str(minute) + "_" + str(second) + ".npy"
+                    np.save(destination_rgb, rgb_video_r)
+
+                    print("Processed video " + str(video_id) + " " + str(hour) + " " + str(minute) + " " + str(second))
+
+                    for t in range(1, 250):
+                        label_finder.readline()
+
+                    if (((minute+1) >= last_min) and (hour == last_hour) and (second == 30)):
+                        video_done = True
+                        break
+                if (video_done == True):
+                    break
+            if (video_done == True):
+                break
+
+def label_decoder(raw_label):
+
+    if(raw_label == "Preparation"):
+        return 0
+    elif(raw_label == "CalotTriangleDissection"):
+        return 1
+    elif(raw_label == "ClippingCutting"):
+        return 2
+    elif(raw_label == "GallbladderDissection"):
+        return 3
+    elif(raw_label == "GallbladderPackaging"):
+        return 4
+    elif(raw_label == "CleaningCoagulation"):
+        return 5
+    elif(raw_label == "GallbladderRetraction"):
+        return 6
+
+def main(_):
+    run_processing()
+
+if __name__ == '__main__':
+    run_processing()
