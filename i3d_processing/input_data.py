007ee29c7b93b75498973d9ac7a47e5968295e6a
Suraj Menon <sjmickiemouse@gmail.com>
2019-06-08 15:08:47 -0700

    ..

--- i3d_processing/input_data.py
+++ i3d_processing/input_data.py
@@ -395,9 +395,14 @@ def import_label_flow_batch2(filename, batch_size, current_sample):
             print("Training video found")
             tmp_rgb = np.load(rgb_txt)
         else:
-            print("Training video doesn't exist!")
-            exists = 0
-            break
+            rgb_txt = "../../chollec80/cholec80_processed_batch2/chollec80_processed_data_full_batch2_imb/" + dirname + ".npy"
+
+            if os.path.isfile(rgb_txt):
+                print("Training video found")
+            else:
+                print("Training video doesn't exist!")
+                exists = 0
+                break
 
         if (i == sample_start):
             rgb_data = tmp_rgb
