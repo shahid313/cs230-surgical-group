7ca3a42af83ca4224f1b309d48002600d910b24a
Suraj Menon <sjmickiemouse@gmail.com>
2019-06-09 14:21:15 -0700

    Something like this to test rgb + flow.

--- i3d_processing/input_test.py
+++ i3d_processing/input_test.py
@@ -271,3 +271,76 @@ def import_label_flow_batch2(filename, batch_size, current_sample):
         np_arr_label = np.zeros((1))
 
     return np_arr_rgb_data, np_arr_flow_data, np_arr_label.reshape(batch_size), exists
+
+def import_label_rgb_flow_batch2(filename, batch_size, current_sample):
+    lines = open(filename, 'r')
+    rgb_data = []
+    flow_data = []
+    label = []
+    exists = 1
+    
+    lines = list(lines)
+
+    lines_len = len(lines)
+
+    sample_start = (current_sample % lines_len)
+    
+    for i in range(sample_start, (sample_start+batch_size)):
+        line = lines[i-1].strip('\n').split()
+        dirname = line[0]
+        tmp_label = line[1]
+        
+        #load the .npy file for rgb
+        rgb_txt = "../../chollec80/chollec80_processed_data_full_batch2_test/" + dirname + ".npy"
+
+        if os.path.isfile(rgb_txt):
+            print("Training video found")
+            tmp_rgb = np.load(rgb_txt)
+        else:
+            print("Training video doesn't exist!")
+            exists = 0
+            break
+
+        if (i == sample_start):
+            rgb_data = tmp_rgb
+        else:
+            rgb_data = np.concatenate((rgb_data, tmp_rgb), axis=0)
+
+        #load the .npy file for flow
+        rgb_txt = "../../chollec80/cholec80_processed_batch2_flow/" + dirname + ".npy"
+
+        if os.path.isfile(rgb_txt):
+            print("Training video found")
+            tmp_rgb = np.load(rgb_txt)
+        else:
+            print("Training video doesn't exist!")
+            exists = 0
+            break
+
+        if (i == sample_start):
+            flow_data = tmp_rgb
+        else:
+            flow_data = np.concatenate((flow_data, tmp_rgb), axis=0)
+        
+        #get the correct label
+        label.append(int(tmp_label))
+    
+    #make the arrays nice
+    if (exists == 1):
+        valid_len = len(rgb_data)
+        pad_len = batch_size - valid_len
+        if pad_len:
+            for i in range(pad_len):
+                rgb_data.append(rgb_data[-1])
+                flow_data.append(flow_data[-1])
+                label.append(int(label[-1]))
+
+        np_arr_rgb_data = np.array(rgb_data).astype(np.float32)
+        np_arr_flow_data = np.array(flow_data).astype(np.float32)
+        np_arr_label = np.array(label).astype(np.int64)
+    else:
+        np_arr_rgb_data = []
+        np_arr_flow_data = []
+        np_arr_label = np.zeros((1))
+
+    return np_arr_rgb_data, np_arr_flow_data, np_arr_label.reshape(batch_size), exists
